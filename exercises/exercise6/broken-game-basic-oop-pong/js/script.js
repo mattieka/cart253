34c1680443801d2013fa91b0cbc528defbf9054e
Mattie <m_alrod@cda055ev5718x.local>
2018-10-26 14:09:57 -0400

    ex6: curly bracket in wrong place
    the very last curly bracket on line 57 enclosed the draw function INSIDE the setup function, which broke the darn thing. i removed it and put it on line 31 instead.

--- exercises/exercise6/broken-game-basic-oop-pong/js/script.js
+++ exercises/exercise6/broken-game-basic-oop-pong/js/script.js
@@ -27,8 +27,8 @@ function setup() {
   // Keycodes 83 and 87 are W and S respectively
   //////////////////////////////////////////////////////// FIXED!!!!!!!
   leftPaddle = new Paddle(0,height/2,10,60,10,83,87);
-
-
+  ////////////////////////////////////////////////////// FIXED!!!!!!!
+}
 // draw()
 // Handles input, updates all the elements, checks for collisions
 // and displays everything.
@@ -54,4 +54,3 @@ function draw() {
   leftPaddle.display();
   ////////////////////////////////////////////////////// FIXED!!!!!!!
   rightPaddle.display();
-}
