cad54611b07394319df075d0d00e660274c45a82
Mattie <m_alrod@cda070ev5815x.local>
2018-10-30 16:19:32 -0400

    P2: finished reformatting paddle.js

--- projects/project2/js/Paddle.js
+++ projects/project2/js/Paddle.js
@@ -1,11 +1,12 @@
-// Paddle
-//
-// A class that defines how a paddle behaves, including the ability
-// to specify the input keys to move it up and down
+/****************************** P A D D L E **********************************/
 
-// Paddle constructor
-//
-// Sets the properties with the provided arguments or defaults
+/* A class to define how a paddle behaves. Includes:
+      - player input (arrow keys and "W"/"S" keys),
+      - moves paddle up or down based on player input */
+
+/************************ C O N S T R U C T O R ******************************/
+
+/* sets constructor properties and their arguments */
 function Paddle(x,y,w,h,speed,downKey,upKey) {
   this.x = x;
   this.y = y;
@@ -18,10 +19,9 @@ function Paddle(x,y,w,h,speed,downKey,upKey) {
   this.upKey = upKey;
 }
 
-// handleInput()
-//
-// Check if the up or down keys are pressed and update velocity
-// appropriately
+/*********************** H A N D L E   I N P U T  ****************************/
+
+/* checks if the up or down keys are pressed and updates velocity accordingly */
 Paddle.prototype.handleInput = function() {
   if (keyIsDown(this.upKey)) {
     this.vy = -this.speed;
@@ -34,17 +34,20 @@ Paddle.prototype.handleInput = function() {
   }
 }
 
-// update()
-// Update y position based on velocity
-// Constrain the resulting position to be within the canvas
+/********************** U P D A T E    F U N C T I O N ***********************/
+
+/* update function does the following:
+      - updates y position based on velocity
+      - constrains the resulting position to be within the canvas */
+
 Paddle.prototype.update = function() {
   this.y += this.vy;
   this.y = constrain(this.y,0,height-this.h);
 }
 
-// display()
-//
-// Draw the paddle as a rectangle on the screen
+/********************* D I S P L A Y   F U N C T I O N  **********************/
+
+// Draws the paddle as a rectangle on the screen
 Paddle.prototype.display = function() {
   fill(255);
   rect(this.x,this.y,this.w,this.h);
