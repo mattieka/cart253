f79c573f1ed98e28c7198370a56b33c4ea677d40
mattie <m_alrod@cda055ev5718x.local>
2018-11-19 12:37:13 -0500

    P3: added sample dialogue in a json file

---
+++ projects/project3/js/Dialogue.js
@@ -0,0 +1,13 @@
+/* -------------------------- DIALOGUE OBJECT ----------------------------- */
+
+//all functions pertaining to testing dialogue
+
+/* ---------------------------- VARIABLE -------------------------------- */
+
+var currentText = 0;
+
+/* --------------------------- SETUP TEXT BOX ------------------------------ */
+
+function setupTextBox() {
+  
+}
